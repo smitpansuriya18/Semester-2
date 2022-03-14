ec727d5f484e78d101665a76749c041640b4190b
smitpansuriya18 <94975254+smitpansuriya18@users.noreply.github.com>
2022-03-14 09:29:25 +0530

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,21 @@
+Select len('smit patel')
+select len('smit')
+
+
+Select len(Firstname),Firstname from Students
+Where len(Firstname)>4
+
+Select len(null)
+Select len('null')
+
+Select len('  ')
+
+Select upper('Smit')
+Select lower('Smit')
+
+select left ('smit_pansuriya',4)
+select right ('smit_pansuriya',9)
+
+Select Substring('Smit',3,10)
+
+Select replace('smit','sm','i')
